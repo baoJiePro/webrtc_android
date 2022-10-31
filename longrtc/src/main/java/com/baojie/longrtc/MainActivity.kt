31786a3ca772ce019a724e12f66b548b5a93ce85
baojie <baojie@1deMacBook-Pro.local>
2022-11-01 00:21:54 +0800

    navigation联动

--- longrtc/src/main/java/com/baojie/longrtc/MainActivity.kt
+++ longrtc/src/main/java/com/baojie/longrtc/MainActivity.kt
@@ -2,19 +2,43 @@ package com.baojie.longrtc
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.baojie.longrtc.activity.ConfigActivity
 import com.baojie.longrtc.base.BaseActivity
 import com.baojie.longrtc.databinding.ActivityMainBinding
 import com.baojie.longrtc.socket.IUserState
+import com.baojie.longrtc.socket.SocketManager
 import com.blankj.utilcode.util.ActivityUtils
 
 class MainActivity : BaseActivity(), IUserState {
     private lateinit var binding: ActivityMainBinding
+    private var isFromCall = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(binding.root)
         binding.lifecycleOwner = this
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.navigation_user,
+            R.id.navigation_room,
+            R.id.navigation_setting
+        ).build()
+        // 設置ActionBar跟随联动
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        // 设置Nav跟随联动
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        SocketManager.addUserStateCallback(this)
+
+
     }
 
 
